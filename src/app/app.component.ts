e91ccde52b8f355927edb83f00f5fb922b4c76a1
Carlos <cagumi@alumnatflorida.es>
2025-02-11 08:50:56 +0100

    Modificando app.components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-<<<<<<< HEAD
-import { FooterComponent } from "./components/footer/footer.component";
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
-=======
 import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { FormStyle } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
@@ -15,7 +8,6 @@ import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,HeaderComponent,CarrouselComponent,FooterComponent],
->>>>>>> master
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
