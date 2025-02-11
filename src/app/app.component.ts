ab420ab172085a737b31398585706dbebfae2598
Carlos <cagumi@alumnatflorida.es>
2025-02-11 11:59:39 +0100

    Cambios en el user-header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,15 @@ import { CarrouselComponent } from './components/carrousel/carrousel.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { CardsComponent } from './components/cards/cards.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,CarrouselComponent,FooterComponent,JumbotronComponent,CardsComponent],
+  imports: [RouterOutlet,HeaderComponent,CarrouselComponent,FooterComponent,JumbotronComponent,CardsComponent, HeaderUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'front';
+  public isUserRegistered: boolean = false;
 }
