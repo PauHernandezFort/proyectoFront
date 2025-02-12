ba00aecaae328451b3bfadcb20781dacbc4e5513
marcos <marcosvallecillosu@gmail.com>
2025-02-12 19:27:30 +0100

    añadiendo mma

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,16 @@ import { schedulesComponent } from './views/schedules/schedules.component';
 import { ContactComponent } from './components/forms/contact/contact.component';
 import { SignInComponent } from './components/forms/sign-in/sign-in.component';
 import { SignUpComponent } from './components/forms/sign-up/sign-up.component';
+import { MmaComponent } from './views/mma/mma.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'activities', component: ActivitiesComponent },
+    { path: 'activities/mma', component: MmaComponent },
     { path: 'schedules', component: schedulesComponent },
     { path: 'formContact', component: ContactComponent },
     { path: 'signIn', component: SignInComponent },
-    { path: 'signUp', component: SignUpComponent }  
+    { path: 'signUp', component: SignUpComponent } 
 ];
