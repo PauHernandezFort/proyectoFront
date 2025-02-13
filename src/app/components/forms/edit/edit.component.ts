12af400b533ad08c35b2d112ea51d9dd7b3e4c5b
Alberto Villegas <alvich@floridauniversitaria.es>
2025-02-13 18:30:24 +0100

    Haciendo edición perfil

--- src/app/components/forms/edit/edit.component.ts
+++ src/app/components/forms/edit/edit.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './edit.component.css'
 })
 export class EditComponent {
+  public photoImage: string = 'https://www.lavanguardia.com/files/og_thumbnail/uploads/2021/03/05/60421be64918d.jpeg';
+
+  previewImage(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.photoImage = reader.result as string;  // Actualizamos la imagen
+      };
+      reader.readAsDataURL(file);  // Leemos el archivo como una URL de datos
+    }
+  }
 
 }
