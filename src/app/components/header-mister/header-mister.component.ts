51d5a7313d180f9f6d72ebcf5819e0139233c735
sintaxyserror <joagsa@floridauniversitaria.es>
2025-02-13 10:40:04 +0100

    cambios varios

---
+++ src/app/components/header-mister/header-mister.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-mister',
+  imports: [],
+  templateUrl: './header-mister.component.html',
+  styleUrl: './header-mister.component.css'
+})
+export class HeaderMisterComponent {
+
+}
