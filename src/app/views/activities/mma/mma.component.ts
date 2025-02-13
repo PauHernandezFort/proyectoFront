5c73131cca44aef23d9f285d61fc0eeeccc679e7
Carlos <cagumi@alumnatflorida.es>
2025-02-13 09:29:40 +0100

    Añadiendo mma

---
+++ src/app/views/activities/mma/mma.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mma',
+  imports: [],
+  templateUrl: './mma.component.html',
+  styleUrl: './mma.component.css'
+})
+export class MmaComponent {
+
+}
