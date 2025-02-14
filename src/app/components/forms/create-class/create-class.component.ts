7ef30a6e1589016e549e202874484008343f8a40
sintaxyserror <joagsa@floridauniversitaria.es>
2025-02-14 11:53:47 +0100

    hacer reactivos los formularios

--- src/app/components/forms/create-class/create-class.component.ts
+++ src/app/components/forms/create-class/create-class.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ApiServiceService } from '../../../services/api-service.service';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-create-class',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-class.component.html',
-  styleUrl: './create-class.component.css'
+  styleUrls: ['./create-class.component.css']
 })
 export class CreateClassComponent {
+  createClass = new FormGroup({
+    nameClass: new FormControl('', Validators.required),
+    teacherName: new FormControl('', Validators.required),
+    capacity: new FormControl('', [Validators.required, Validators.min(1)]),
+    classInfo: new FormControl(''),
+    classDate: new FormControl('', Validators.required),
+    classStatus: new FormControl('', Validators.required),
+  });
 
+
+  onClick(): void {
+    console.log('Formulario válido:', this.createClass.valid);
+    if (this.createClass.valid) {
+      this.newClass();
+    } else {
+      console.log('Por favor, completa todos los campos correctamente');
+    }
+  }
+  
+  newClass() {
+    console.log('Clase creada');
+  }
+  
+  
 }
