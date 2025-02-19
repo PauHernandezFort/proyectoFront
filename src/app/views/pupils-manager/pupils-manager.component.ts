36bc363c7f1e5af9bb293708ed1ff28ec40d33c7
Alberto Villegas <alvich@floridauniversitaria.es>
2025-02-19 13:12:06 +0100

    Realizando progreso

--- src/app/views/pupils-manager/pupils-manager.component.ts
+++ src/app/views/pupils-manager/pupils-manager.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api-service.service';
+import { Usuarios as Member } from '../../models/user.interface'; // Asegúrate de que la interfaz es correcta
 
 @Component({
   selector: 'app-pupils-manager',
@@ -9,14 +11,38 @@ import { CommonModule } from '@angular/common';
   templateUrl: './pupils-manager.component.html',
   styleUrl: './pupils-manager.component.css'
 })
+
 export class PupilsManagerComponent {
   loading: { [key: string]: boolean } = {};
+  public trainers: Member[] = [];
+  public id: number = 0;
 
-  eliminarEntrenador(id: string) {
+  constructor(public service: ApiService) { }
+
+  ngOnInit(): void {
+    this.getResponseTrainers();
+  }
+
+  public getResponseTrainers(): void {
+    this.service.getResponsePupils().subscribe(
+      (response) => {
+        response.forEach((member) => {
+          if (member.rol === "entrenador") {
+            this.trainers.push(member);
+          }
+        });
+        console.log(this.trainers);
+        //this.trainers = response.filter((user: Usuarios) => user.rol === "Entrenador");
+      },
+      (error) => {
+        console.error("Error al obtener los entrenadores:", error);
+      }
+    );
+  }
+
+  deleteTrainer(id: string) {
     if (confirm('¿Estás seguro de que deseas eliminar este entrenador?')) {
       this.loading[id] = true;
-      
-      // Simulamos la eliminación con un timeout
       setTimeout(() => {
         this.loading[id] = false;
         alert('Entrenador eliminado correctamente');
